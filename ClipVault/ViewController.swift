1248c0b0ebb8145424b076a434b9de18c716ee49
benyaover <benyavahdat@gmail.com>
2025-03-24 12:36:28 -0700

    Initial Commit

---
+++ ClipVault/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClipVault
+//
+//  Created by Benya Vahdat on 3/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ClipVaultTests/ClipVaultTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipVaultTests.swift
+//  ClipVaultTests
+//
+//  Created by Benya Vahdat on 3/24/25.
+//
+
+import Testing
+@testable import ClipVault
+
+struct ClipVaultTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
